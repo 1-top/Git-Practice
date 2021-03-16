f0f659410fc29023d6cc18067935216124b34fe5
Harry-Bro <likeharrybro@gmail.com>
2021-03-16 16:52:29 +0900

    fix: Conflict Resolution

--- src/main/java/Dong.java
+++ src/main/java/Dong.java
@@ -1,6 +1,7 @@
 public class Dong {
+
     public static void main(String[] args) {
-        System.out.println("123456");
-        System.out.println("hihi");
+        System.out.println("test");
     }
+
 }
