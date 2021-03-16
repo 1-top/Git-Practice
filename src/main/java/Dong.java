acf2dc2e5e280c0ed416913ce89e59bd1ff1b476
codeMonkey-shin <sdw0711@daum.net>
2021-03-16 17:08:22 +0900

    feat: Bug Fix
    bug fixed

--- src/main/java/Dong.java
+++ src/main/java/Dong.java
@@ -1,6 +1,6 @@
 public class Dong {
     public static void main(String[] args) {
         System.out.println("123456");
-        System.out.println("hihi");
+        System.out.println("hihi123");
     }
 }
